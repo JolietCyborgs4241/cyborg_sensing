68d2ea51e23946909d003544f4e54defe6155bd3
cgzog <chris.herzog@gmail.com>
2018-11-30 22:02:00 -0600

    Added comments for doxygen

--- cv_proc/globals.c
+++ cv_proc/globals.c
@@ -11,12 +11,36 @@
 #include "cv_cam.h"
 
 
+/// Program name
+///
+/// argv[0] in Linux terms
+///
+/// Used as part of error and warning messages to identify the program
+/// which generate the message in case you are looking at multiple
+/// debug or log files
 char        *MyName;
 
+/// Network connectivity information
+///
+/// addresses, ports, socket, etc.
 HOST_INFO   HostInfo;
 
+/// Time to Live
+///
+/// How long any sensor data records are kept around before being purged
+///
+/// Who cares what was seen 30 seconds ago?
+///
+/// Typical values are in the low integer number of seconds
 int         Ttl = TTL_DEFAULT;
 
+/// Global debug level
+///
+/// DEBUG_OFF, DEBUG_INFO, DEBUG_DETAIL
 int         DebugLevel = DEBUG_OFF;
+
+/// Global FILE * for debug output
+///
+/// it's standard error unless it gets as part of the command line (-d option)
 FILE        *DebugFP;                               // set to stderr at startup
 
