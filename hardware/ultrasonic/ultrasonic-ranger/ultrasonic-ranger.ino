caeae7008a3415ef714aa89028e5472d932517ab
cgzog <chris.herzog@gmail.com>
2018-12-02 00:43:40 -0600

    Initial commit for ultrasonic ranger code
    Arduino-based ultrasonic ranger.
    
    Uses HS-SR04 cheap-o ultrasonic sensors.
    
    Running an an Arduino Uno now but will end up on a Nano with a small custom PCB supporting the ultrasonic sensor and an 8 position DIP switch for configuration (DIP switch support not in right now)

---
+++ hardware/ultrasonic/ultrasonic-ranger/ultrasonic-ranger.ino
@@ -0,0 +1,65 @@
+    /*
+    * Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+    *
+    * by Dejan Nedelkovski,
+    * www.HowToMechatronics.com
+    *
+    */
+    
+#define TRIG_PIN          10
+#define ECHO_PIN          11
+#define ACTIVE_PIN        13
+
+#define INTER_READ_PAUSE  500     // 1/2 second
+
+
+long duration;
+int  distanceCm;
+
+int activeState;
+
+
+
+void setup() {
+  pinMode(TRIG_PIN, OUTPUT);      // Sets the TRIG_PIN as an Output
+  pinMode(ECHO_PIN, INPUT);       // Sets the ECHO_PIN as an Input
+
+  pinMode(ACTIVE_PIN, OUTPUT);  // flips each time a range happens
+
+  activeState = 0;
+  
+  Serial.begin(9600); // Starts the serial communication
+}
+
+
+
+void loop() {
+  // Clears the TRIG_PIN
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the TRIG_PIN on HIGH state for 10 micro seconds
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+    
+  // Reads the ECHO_PIN, returns the sound wave travel time in microseconds
+  duration = pulseIn(ECHO_PIN, HIGH);
+    
+  // Calculating the distance
+  distanceCm = duration*0.034/2;
+
+  Serial.print("Distance: ");
+  Serial.print(distanceCm);
+  Serial.println(" (cm)");
+
+  if (activeState) {
+    digitalWrite(ACTIVE_PIN, LOW);  // turn the light off
+    activeState = 0;
+  } else {
+    digitalWrite(ACTIVE_PIN, HIGH); // turn light on
+    activeState = 1;
+  }
+
+  delay(INTER_READ_PAUSE);        // wait before the next reading
+}
