d0cfcfbd794c624e852867c7a7ce4dfea47f8e5f
cgzog <chris.herzog@gmail.com>
2018-12-11 17:17:56 -0600

    Hardware commits
    * Gerber files for HC-SR04 ultrasonic ranger board
    
    * renamed Arduino project file to match directory name (keep the Arduino IDE happy)

--- hardware/ultrasonic-ranger_hc-sr04/ultrasonic-ranger_hc-sr04.ino
+++ hardware/ultrasonic-ranger_hc-sr04/ultrasonic-ranger_hc-sr04.ino
@@ -7,12 +7,19 @@
  * www.HowToMechatronics.com
  *
  */
-    
+
+#define ID_BIT0_PIN       4
+#define ID_BIT1_PIN       3
+#define ID_BIT2_PIN       2
+
+#define OFFSET_BIT0_PIN   9
+
 #define TRIG_PIN          10
 #define ECHO_PIN          11
+
 #define ACTIVE_PIN        13
 
-#define INTER_READ_PAUSE  250     // 1/4 second
+#define INTER_READ_PAUSE  100     // 1/10 second
 
 #define OUTPUT_BAUD_RATE  115200
 
@@ -23,9 +30,19 @@ int activeState;
 
 
 void setup() {
-  pinMode(TRIG_PIN, OUTPUT);      // Sets the TRIG_PIN as an Output
-  pinMode(ECHO_PIN, INPUT);       // Sets the ECHO_PIN as an Input
+  //i id setting pins
+  pinMode(ID_BIT0_PIN, INPUT_PULLUP);
+  pinMode(ID_BIT1_PIN, INPUT_PULLUP);
+  pinMode(ID_BIT2_PIN, INPUT_PULLUP);
 
+  // offset setting pins
+  
+  
+  // ultrasonic sensor control pins
+  pinMode(TRIG_PIN, OUTPUT);      // sets the TRIG_PIN as an Output
+  pinMode(ECHO_PIN, INPUT);       // sets the ECHO_PIN as an Input
+
+  // heartbeat LED pin
   pinMode(ACTIVE_PIN, OUTPUT);    // flips each time a range happens
 
   activeState = 0;
@@ -36,9 +53,28 @@ void setup() {
 
 
 void loop() {
-  int   distanceCm;
-  int   duration;
-    
+  int   distanceCm, duration, offset, id;
+
+  // we check the id and offset each time we read the sensor in case it got changed
+  //
+  // one weirdness - to cut down on parts, the switches will actually change the 
+  // input to LOW meaning the pin is "active" (and shoudl be considered a '1' bit
+  // for the purpose of setting values
+
+  // get ID - 3 pins
+
+  id = 0;       // clear all bits
+
+  if (digitalRead(ID_BIT0_PIN) == LOW) {
+    id = id | 0b00000001;
+  }
+  if (digitalRead(ID_BIT1_PIN) == LOW) {
+    id = id | 0b00000010;
+  }
+  if (digitalRead(ID_BIT2_PIN) == LOW) {
+    id = id | 0b00000100;
+  }
+
   // Clears the TRIG_PIN
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
@@ -74,12 +110,14 @@ void loop() {
   // We know the speed of sound (roughly), the time it took to cover some
   // distance, and then using the power of math, we can calculate how far
   // of a distance the sound wave covered
-  //
+
   distanceCm = duration * SPEED_OF_SOUND_CM_PER_USEC / 2;
 
-  Serial.print("Distance: ");
-  Serial.print(distanceCm);
-  Serial.println(" (cm)");
+  // output string should look like "R <id> x <distancecm (adjusted)>"
+  Serial.print("R ");
+  Serial.print(id);
+  Serial.print(" x ");
+  Serial.println(distanceCm);
 
   if (activeState) {
     digitalWrite(ACTIVE_PIN, LOW);  // turn the light off
