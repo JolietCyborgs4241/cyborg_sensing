de15322e2b4b6e895526cf2c67730d950895c732
Chris Herzog <chris.herzog@gmail.com>
2018-12-11 12:11:16 -0600

    More details for run_tests.sh
    Give more details on time elapsed and other time-related stats

--- software/test/run_tests.sh
+++ software/test/run_tests.sh
@@ -30,8 +30,11 @@ MSG_COUNT=${#MSGS[@]}
 
 echo `basename $0`: $MSG_COUNT messages to be sent to ${HOST}:${PORT} via UDP
 echo
-echo "Starting @ `date`"
 echo "--------------------------------------------------------------"
+
+START_DATE_TIME=`date`
+
+START_TIME=`date +%s`
 
 for (( i=0; i <${MSG_COUNT}; i++ ));
 do
@@ -45,13 +48,19 @@ do
     sleep "0.${DELAY}"
 done
 
-TOTAL_DELAY_CHAR_LEN=`echo -n ${TOTAL_DELAY} | wc -c`
+END_TIME=`date +%s`
 
+TOTAL_DELAY_CHAR_LEN=`echo -n ${TOTAL_DELAY} | wc -c`
 TOTAL_DELAY_MSECS_OFFSET=`expr ${TOTAL_DELAY_CHAR_LEN} - 2`
+TOTAL_DELAY_SECS=`expr ${TOTAL_DELAY} / 1000`
+TOTAL_DELAY_SUBSECS=`echo ${TOTAL_DELAY} | cut -c ${TOTAL_DELAY_MSECS_OFFSET}-`
 
 echo "--------------------------------------------------------------"
 echo
-echo -n "Total Delay of `expr ${TOTAL_DELAY} / 1000`"
-echo -n '.'
-echo "`echo ${TOTAL_DELAY} | cut -c ${TOTAL_DELAY_MSECS_OFFSET}-` seconds"
+echo "Started @ ${START_DATE_TIME}"
+echo
 echo "Finished @ `date`"
+echo
+echo "Elapsed time for this test was `expr \( ${END_TIME} - ${START_TIME} \) / 60` min `expr \( ${END_TIME} - ${START_TIME} \) % 60` sec"
+echo
+echo "Total inter-packet delays were ${TOTAL_DELAY_SECS}.${TOTAL_DELAY_SUBSECS}secs using a max inter-packet delay of ${MAX_DELAY}ms"
