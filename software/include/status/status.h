894dfb65c0b5eb5890a46ee9bedbfe8e35e63186
Chris Herzog <chris.herzog@gmail.com>
2018-12-20 14:21:14 -0600

    cyborgauto library status display routines
    first check in

--- software/include/status/status.h
+++ software/include/status/status.h
@@ -21,6 +21,61 @@
 #define STAT_LED_EXTRA_2            '9'
 
 
+#define COLOR_RED_OFF               'X'
+#define COLOR_RED_1                 'a'
+#define COLOR_RED_2                 'b'
+#define COLOR_RED_3                 'c'
+#define COLOR_RED_4                 'd'
+#define COLOR_RED_5                 'e'
+#define COLOR_RED_6                 'f'
+#define COLOR_RED_7                 'g'
+#define COLOR_RED_8                 'h'
+#define COLOR_RED_9                 'i'
+#define COLOR_RED_10                'j'
+#define COLOR_RED_11                'k'
+#define COLOR_RED_12                'l'
+#define COLOR_RED_13                'm'
+#define COLOR_RED_14                'n'
+#define COLOR_RED_15                'o'
+#define COLOR_RED_MAX               'M'
+
+#define COLOR_GREEN_OFF             COLOR_RED_OFF
+#define COLOR_GREEN_1               COLOR_RED_1
+#define COLOR_GREEN_2               COLOR_RED_2
+#define COLOR_GREEN_3               COLOR_RED_3
+#define COLOR_GREEN_4               COLOR_RED_4
+#define COLOR_GREEN_5               COLOR_RED_5
+#define COLOR_GREEN_6               COLOR_RED_6
+#define COLOR_GREEN_7               COLOR_RED_7
+#define COLOR_GREEN_8               COLOR_RED_8
+#define COLOR_GREEN_9               COLOR_RED_9
+#define COLOR_GREEN_10              COLOR_RED_10
+#define COLOR_GREEN_11              COLOR_RED_11
+#define COLOR_GREEN_12              COLOR_RED_12
+#define COLOR_GREEN_13              COLOR_RED_13
+#define COLOR_GREEN_14              COLOR_RED_14
+#define COLOR_GREEN_15              COLOR_RED_15
+#define COLOR_GREEN_MAX             COLOR_RED_MAX
+
+#define COLOR_BLUE_OFF              COLOR_RED_OFF
+#define COLOR_BLUE_1                COLOR_RED_1
+#define COLOR_BLUE_2                COLOR_RED_2
+#define COLOR_BLUE_3                COLOR_RED_3
+#define COLOR_BLUE_4                COLOR_RED_4
+#define COLOR_BLUE_5                COLOR_RED_5
+#define COLOR_BLUE_6                COLOR_RED_6
+#define COLOR_BLUE_7                COLOR_RED_7
+#define COLOR_BLUE_8                COLOR_RED_8
+#define COLOR_BLUE_9                COLOR_RED_9
+#define COLOR_BLUE_10               COLOR_RED_10
+#define COLOR_BLUE_11               COLOR_RED_11
+#define COLOR_BLUE_12               COLOR_RED_12
+#define COLOR_BLUE_13               COLOR_RED_13
+#define COLOR_BLUE_14               COLOR_RED_14
+#define COLOR_BLUE_15               COLOR_RED_15
+#define COLOR_BLUE_MAX              COLOR_RED_MAX
+
+
 /// status message to status server consists of 4 bytes
 ///
 /// LED   R value   G value   B value
@@ -39,13 +94,28 @@
 /// 48-63   -> 'd'
 /// ...
 /// 16-239   -> 'o'
-/// 240-255   -> 'p'
+/// 240-255  -> 'M'
+///
+/// protocol to driver hardware brackets the 4 bytes with '<' and '>'
+///
+/// this allows us to detect a potetial framing error and recover from it
+
+
 
 /// \brief send status update to status server
 ///
 /// LED, R, G, B values
-void    sendStatusUpdate(char, int, int, int);
+///
+/// returns 0 if error - otherwise returns number of bytes sent (should be 4)
+int    sendStatusUpdate(char, char, char, char);
 
+
+
+/// \brief  open connection to status server
+///
+/// since actual connection is UDP, it just creates the socket so no
+/// actual "connection" is established
+void   openStatusConnection(HOST_INFO *host);
 
 
 typedef struct {
---
+++ software/lib/status.c
@@ -0,0 +1,77 @@
+//  status.c   cyborg autonomous status display update routines
+//
+//  intended to be called from programs to update their status
+//
+//      - send status to status server (non-blocking)
+//      - status server talks to status bar driver hardware
+//        - Arduino-based
+//        - Communicates over serial protocol
+
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+
+#include "cv_net.h"
+#include "status/status.h"
+
+
+static  HOST_INFO   localHostInfoCopy;
+
+
+void    *cvAlloc();
+
+
+/// \brief open a status connection
+///
+/// allocates space for a local copy of the ASCII host address
+void
+openStatusConnection(HOST_INFO *host)
+{
+    openOutgoingPort(host);
+
+    localHostInfoCopy = *host;
+
+    localHostInfoCopy.hostIPString = cvAlloc(strlen(host->hostIPString) + 1);
+    strcpy(localHostInfoCopy.hostIPString, host->hostIPString);
+}
+
+
+
+
+
+/// \brief  send an update for a single led to the status display server
+///
+/// led to set
+/// rgb value to
+int
+sendStatusUpdate(char led, char red, char green, char blue)
+{
+    int sockRet;
+    STATUS_MSG  status;
+
+    status.led   = led;
+    status.red   = red;
+    status.green = green;
+    status.blue  = blue;
+
+    if ((sockRet = sendto(localHostInfoCopy.sock, &status, sizeof(status), 0,
+                          (struct sockaddr *)&localHostInfoCopy.hostIP,
+                          sizeof(struct sockaddr_in))) == -1) {
+
+        return 0;           // error !
+    }
+
+    return sockRet;         // succcess
+}
+
