f3ecfb525a26952e5d40aa652de0cb0f9a72a252
cgzog <chris.herzog@gmail.com>
2019-02-07 20:42:01 -0600

    Added live video support
    python3 jvem.py -s static_image_file
    
    python3 jvem.py -v video_capture_file
    
    python3 jvem.py -c camera_id

--- r+d/jevois_python_emulator/jvem.py
+++ r+d/jevois_python_emulator/jvem.py
@@ -6,34 +6,68 @@ import Reflectors as rf
 
 
 
+def usage():
+    print("Usage: " + sys.argv[0] + ": [-h] | [-s static_image_file] | [-v video_capture_file] | [-c camera_id]")
 
 
-# there need to be at least 2 command line parameters (the program name counts as one)
+# initialize and document a few key variables
+
+MODE_STATIC_FILE = "static"
+MODE_VIDEO_FILE  = "video"
+MODE_CAMERA      = "camera"
+
+WINDOW_NAME = "JeVois Emulator"
+
+mode         = ""    # will be either "static", "video", or "camera"
+
+static_file  = ""    # static image source file
+
+video_source = ""    # video source - file or camera
+
+
+# check and setup operational parameters off of command line
 #
-# the parameter is the name of the static image file used for the image processing
+# I swear, one more command line option and we'll move to getopt
 
-if (len(sys.argv)) < 2 or (sys.argv[1] == "-v" and len(sys.argv) < 3):
-    print("Usage: " + sys.argv[0] + ": [image] | -v [video capture]")
+if len(sys.argv) == 2 and sys.argv[1] == "-h":
+
+    usage()
+    sys.exit(0)     # exit with 0 since -h is a valid option
+
+
+if sys.argv[1] == "-s" and len(sys.argv) == 3:
+
+    mode = MODE_STATIC_FILE
+    static_file = sys.argv[2]
+
+elif sys.argv[1] == "-v" and len(sys.argv) == 3:
+
+    mode = MODE_VIDEO_FILE
+    video_source = sys.argv[2]
+
+elif sys.argv[1] == "-c" and len(sys.argv) == 3:
+
+    mode = MODE_CAMERA
+    video_source = int(sys.argv[2])
+
+else:   # unknown command line option!!
+
+    print("Error: " + sys.argv[0] + ": invalid or missing command line parameters - use -h for help")
+    usage()
     sys.exit(1)
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-    mode = "image"
-else:
-    filename = sys.argv[2]
-    mode = "video"
 
-
-window_name = "JeVois Emulator"
-
+print("mode = '" + mode + "'")
 
 # create the output object for the Jevois-targeted code and give it the
 # name we'll use to reference the window to display any output it
-jv_out = jevois.Jevois_out(window_name)
+
+jv_out = jevois.Jevois_out(WINDOW_NAME)
 
 
 # create the named window
-cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+
+cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
 
 
 # create an instance of the class we plan to run on the Jevois camera
@@ -75,18 +109,18 @@ cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 reflector = rf.Reflectors()
 
 
-if mode == "image":
+if mode == MODE_STATIC_FILE:    # static file processing
 
     #read the image
-    image = cv2.imread(sys.argv[1], cv2.IMREAD_UNCHANGED)
+    image = cv2.imread(static_file, cv2.IMREAD_UNCHANGED)
 
     reflector.process(image, jv_out)    # we only need to call it once
 
     cv2.waitKey(0)
 
-else:   # video mode
+elif mode == MODE_VIDEO_FILE or mode == MODE_CAMERA:   # video file processing
 
-    video = cv2.VideoCapture(sys.argv[2])
+    video = cv2.VideoCapture(video_source)
 
     count = 0
 
@@ -105,8 +139,15 @@ else:   # video mode
 
         reflector.process(frame, jv_out)
 
+else:   # what mode is this???
 
-if mode == "video":
+    print(sys.argv[0] + ": error: unknown mode '" + mode +"'")
+    sys.exit(1)
+
+
+# cleanup as needed
+
+if mode == MODE_VIDEO_FILE or mode == MODE_CAMERA:
 
     video.release()
 
